ca383ae97fee2b1d872fb1ab0708e2c23ef2bae3
Felipe Martina <felipe.martina@3xmgroup.com>
2016-12-12 15:58:08 -0300

    Some clean up and removed unnecessary empy sms

--- checkPrices.js
+++ checkPrices.js
@@ -1,58 +1,57 @@
-var util = require("util");
-var AWS = require('aws-sdk');
-var sns = new AWS.SNS();
+var util = require('util')
+var AWS = require('aws-sdk')
+var sns = new AWS.SNS()
 module.exports.handle = (event, context, callback) => {
-  console.log(JSON.stringify(event, null, 2));
+  console.log(JSON.stringify(event, null, 2))
   // store flight prices in an array
-  var smsMessage = '';
+  var smsMessage = ''
   // iterate DynamoDB events
   event.Records.forEach((record) => {
-    if (record.eventName === 'INSERT') {// we only use INSERT events
-      var flightRecord = record.dynamodb.NewImage;
-      var flight = flightRecord.flight.S.toUpperCase();
-      var origin = flight.substring(0, 3);
-      var date = flightRecord.date.S.substring(0, 10);
-      var price = flightRecord.price.S;
-      console.log(util.format('origin: %s, date: %s, price: %s', origin, date, price));
+    if (record.eventName === 'INSERT') { // we only use INSERT events
+      var flightRecord = record.dynamodb.NewImage
+      var flight = flightRecord.flight.S.toUpperCase()
+      var origin = flight.substring(0, 3)
+      var date = flightRecord.date.S.substring(0, 10)
+      var price = flightRecord.price.S
+      console.log(util.format('origin: %s, date: %s, price: %s', origin, date, price))
       switch (origin) {
         case 'COR':
           if (price < 'USD1550') {
-            smsMessage += util.format('- %s %s in %s\n', flight, price, date);
+            smsMessage += util.format('- %s %s in %s\n', flight, price, date)
           }
-          break;
+          break
 
         case 'SCL':
           if (price < 'USD1250') {
-            smsMessage += util.format('- %s %s in %s\n', flight, price, date);
+            smsMessage += util.format('- %s %s in %s\n', flight, price, date)
           }
-          break;
+          break
       }
     }
-  });
+  })
 
   // Check if any cheap flight was found
-  if (smsMessage === '') smsMessage = 'No cheap flights found today :(';
-
-  // Send SMS with the price updates
-  var params = {
-    Message: smsMessage,
-    PhoneNumber: process.env.PHONE,
-    MessageAttributes:{
-      SMSType:{
-        DataType: "String",
-        StringValue: "Transactional"
+  if (smsMessage !== '') {
+    // Send SMS with the price updates
+    var params = {
+      Message: smsMessage,
+      PhoneNumber: process.env.PHONE,
+      MessageAttributes: {
+        SMSType: {
+          DataType: 'String',
+          StringValue: 'Transactional'
+        }
       }
     }
-  };
-  console.log(JSON.stringify(params, null, 2));
-  sns.publish(params, (err, data) => {
-    if (err) {
-      console.log(err, err.stack); // an error occurred
-      callback(err);
-    }
-    else {
-      console.log('Message sent: %s', smsMessage); // successful response
-      callback(null, smsMessage);
-    }
-  });
-};
+    console.log(JSON.stringify(params, null, 2))
+    sns.publish(params, (err, data) => {
+      if (err) {
+        console.log(err, err.stack) // an error occurred
+        callback(err)
+      } else {
+        console.log('Message sent: %s', smsMessage) // successful response
+        callback(null, smsMessage)
+      }
+    })
+  }
+}
--- fetchPrices.js
+++ fetchPrices.js
@@ -1,98 +1,98 @@
-"use strict";
-var rp = require("request-promise");
-var util = require("util");
-var Promise = require("bluebird");
-var AWS = require('aws-sdk');
-AWS.config.update({ region: "us-east-1" });
-var dynamodb = new AWS.DynamoDB.DocumentClient();
+'use strict'
+var rp = require('request-promise')
+var util = require('util')
+var Promise = require('bluebird')
+var AWS = require('aws-sdk')
+AWS.config.update({ region: 'us-east-1' })
+var dynamodb = new AWS.DynamoDB.DocumentClient()
 
 /* CONSTANTS */
-var googleQPX = "https://www.googleapis.com/qpxExpress/v1/trips/search?key={{API_KEY}}";
-const API_KEY = process.env.API_KEY;
-const DYNAMODB_TABLE = process.env.DYNAMODB_TABLE;
-var maxPrice = 'USD1300';
+var googleQPX = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key={{API_KEY}}'
+const API_KEY = process.env.API_KEY
+const DYNAMODB_TABLE = process.env.DYNAMODB_TABLE
+var maxPrice = 'USD1300'
 
 module.exports.handle = (event, context, callback) => {
   // Check prices to fetch
-  var flights;
+  var flights
   if (process.env.FLIGHTS) {
-    flights = JSON.parse(jsonFlights);
+    flights = JSON.parse(process.env.FLIGHTS)
   } else {
     flights = [{
-      "origin": "SCL",
-      "destination": "HNL",
-      "dates": ["2017-01-05", "2017-01-06", "2017-01-07", "2017-01-08", "2017-01-09", "2017-01-10", "2017-01-11"]
-      //"dates": ["2017-01-05", "2017-01-06"]
+      'origin': 'SCL',
+      'destination': 'HNL',
+      'dates': ['2017-01-05', '2017-01-06', '2017-01-07', '2017-01-08', '2017-01-09', '2017-01-10', '2017-01-11']
+      // "dates": ["2017-01-05", "2017-01-06"]
     }, {
-      "origin": "COR",
-      "destination": "HNL",
-      "dates": ["2017-01-05", "2017-01-06", "2017-01-07", "2017-01-08", "2017-01-09", "2017-01-10", "2017-01-11"]
-      //"dates": ["2017-01-05", "2017-01-06"]
-    }];
+      'origin': 'COR',
+      'destination': 'HNL',
+      'dates': ['2017-01-05', '2017-01-06', '2017-01-07', '2017-01-08', '2017-01-09', '2017-01-10', '2017-01-11']
+      // "dates": ["2017-01-05", "2017-01-06"]
+    }]
   }
 
   // create a list of promises for each origin and for each date
-  var promises = [];
+  var promises = []
   flights.forEach((flight) => {
     promises.push.apply(promises, flight.dates.map(date => {
       return getFlights(flight.origin, flight.destination, date, maxPrice)
         .then(processFlightsResponse)
         .catch(err => {
-          console.log(err);
-        });
-    }));
-  });
+          console.log(err)
+        })
+    }))
+  })
   // Wait for all request to be complete and processed
   Promise.all(promises).then(results => {
-    callback(null, "Flight prices saved in DynamoDB");
+    callback(null, 'Flight prices saved in DynamoDB')
   }).catch(err => {
-    callback(err);
-  });
-};
+    callback(err)
+  })
+}
 
-function getFlights(origin, destination, date, maxPrice) {
+function getFlights (origin, destination, date, maxPrice) {
   var reqOptions = {
-    uri: googleQPX.replace("{{API_KEY}}", API_KEY),
+    uri: googleQPX.replace('{{API_KEY}}', API_KEY),
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     },
     json: true, // Automatically parses the JSON string in the response
-    method: "POST",
+    method: 'POST',
     body: {
-      "request": {
-        "passengers": {
-          "kind": "qpxexpress#passengerCounts",
-          "adultCount": 1
+      'request': {
+        'passengers': {
+          'kind': 'qpxexpress#passengerCounts',
+          'adultCount': 1
         },
-        "slice": [
+        'slice': [
           {
-            "kind": "qpxexpress#sliceInput",
-            "origin": origin,
-            "destination": destination,
-            "date": date
+            'kind': 'qpxexpress#sliceInput',
+            'origin': origin,
+            'destination': destination,
+            'date': date
           }
         ],
         // "maxPrice": maxPrice, // TODO - Max price removed
-        "solutions": 1, // Take only 1 flight (the cheapest) 
-        "saleCountry": "US" // Force prices to come in US
+        'solutions': 1, // Take only 1 flight (the cheapest)
+        'saleCountry': 'US' // Force prices to come in US
       }
     }
-  };
-  return rp(reqOptions);
+  }
+  return rp(reqOptions)
 }
 
-function processFlightsResponse(response) {
+function processFlightsResponse (response) {
   return new Promise((resolve, reject) => {
-    console.log(response);
+    console.log(response)
     // check if we have any trip that meets the parameters in the request
     if (!response.trips.tripOption) {
-      resolve(null);
+      resolve(null)
     }
     // get the cheapest flight
     var cheapest = response.trips.tripOption.reduce((cheapestFlight, flight) => {
-      cheapestFlight = cheapestFlight || flight;
-      return flight.saleTotal < cheapestFlight.saleTotal ? flight : cheapestFlight;
-    });
+      cheapestFlight = cheapestFlight || flight
+      return flight.saleTotal < cheapestFlight.saleTotal ? flight : cheapestFlight
+    })
     // Insert flight in DynamoDB table
     var flight = {
       timestamp: new Date().toISOString(),
@@ -100,14 +100,14 @@ function processFlightsResponse(response) {
       date: cheapest.slice[0].segment[0].leg[0].departureTime,
       price: cheapest.saleTotal,
       duration: Math.round(cheapest.slice[0].duration / 60) // duration is expressed in minutes, lets store it in hours
-    };
+    }
     var params = {
       TableName: DYNAMODB_TABLE,
       Item: flight
-    };
+    }
     dynamodb.put(params, (err, data) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
+      if (err) reject(err)
+      else resolve()
+    })
+  })
 }
